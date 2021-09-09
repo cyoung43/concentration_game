4c4e02bd6477d8e904a8722a9353eaa80ad99a61
Chris Young <cyoung.byu@gmail.com>
2021-09-08 18:20:32 -0600

    Initial Commit

---
+++ ConcentrationGame/ConcentrationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationGameApp.swift
+//  ConcentrationGame
+//
+//  Created by New User on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
