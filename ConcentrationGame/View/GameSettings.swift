59ef4d1b1af5792d862c575e35b67d8c815724cf
Chris Young <cyoung43@byu.edu>
2021-10-05 13:34:20 -0600

    added in game settings file

---
+++ ConcentrationGame/View/GameSettings.swift
@@ -0,0 +1,20 @@
+//
+//  GameSettings.swift
+//  ConcentrationGame
+//
+//  Created by Chris Young on 10/5/21.
+//
+
+import SwiftUI
+
+struct GameSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        GameSettings()
+    }
+}
