9ad0b3a7d8517eeff46cbb16549bacb6a74d6d6a
Chris Young <cyoung43@byu.edu>
2021-10-04 20:09:02 -0600

    getting the filter ready

--- ConcentrationGame/View/GameMode.swift
+++ ConcentrationGame/View/GameMode.swift
@@ -2,7 +2,37 @@
 //  GameMode.swift
 //  ConcentrationGame
 //
-//  Created by Chris Young on 10/4/21.
+//  Created by New User on 10/4/21.
 //
 
 import SwiftUI
+
+struct GameMode: View {
+    var gameType: GameType
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Game Modes")) {
+                    ForEach (themes) { theme in
+                        if theme.gameType == gameType {
+                            NavigationLink(theme.name, destination: { EmojiGameView(emojiGame: EmojiConcentrationGame())
+                                    .navigationBarBackButtonHidden(true)
+                                    .navigationBarHidden(true)
+                                
+                                // TO DO: Make back button go to the emoji mojo game modes, temple match game modes, etc... Currently goes to the home screen
+                            })
+                        }
+                    }
+                }
+            }
+                .navigationTitle("Emoji Mojo")
+        }
+    }
+}
+
+struct GameMode_Previews: PreviewProvider {
+    static var previews: some View {
+        GameMode(gameType: .emojiMojo)
+    }
+}
